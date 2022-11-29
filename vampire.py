05c3110955b25d8e879e37d0d2c2a5a6ae40fcfd
rainy-river <jme.96@outlook.com>
2022-11-28 23:22:44 -0500

    fourth commit

--- vampire.py
+++ vampire.py
@@ -3,8 +3,6 @@ age = 3000
 if name == 'Alice':
     print('Hi, Alice.')
 elif age < 12:
-    print('You are not Alice, kiddo.')
-elif age > 2000:
-    print('Unlike you, Alice is not an undead, immortal vampire.')
-elif age > 100:
-    print('You are not Alice, grannie.')
+    print('You are not Alice, kiddo')
+else:
+    print('You are neither Alice nor a little kid.')
