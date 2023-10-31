3d316ee0a2ec1ef0fd5f82b5a9c2fbacf6abed77
rainy-river <jme.96@outlook.com>
2023-10-30 22:34:46 -0400

    sync

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -4,6 +4,7 @@ print('ROCK, PAPER, SCISSORS')
 wins = 0
 losses = 0
 ties = 0
+
 while True: # The main game loop.
     print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
     while True: # The player input loop.
@@ -15,45 +16,45 @@ while True: # The main game loop.
             break # Break out of the player input loop.
         print('Type one of r, p, s, or q.')
 
-# Display what the player chose:
-if playerMove == 'r':
-    print('ROCK versus...')
-elif playerMove == 'p':
-    print('PAPER versus...')
-elif playerMove == 's':
-    print('SCISSORS versus...')
+    # Display what the player chose
+    if playerMove == 'r':
+        print('ROCK versus...')
+    elif playerMove == 'p':
+        print('PAPER versus...')
+    elif playerMove == 's':
+        print('SCISSORS versus...')
 
-# Display what the computer chose
-# randomNumber = random.randint(1, 3)
-if randomNumber == 1:
-    computerMove = 'r'
-    print('ROCK')
-elif randomNumber == 2:
-    computerMove = 'p'
-    rint('PAPER')
-elif randomNumber == 3:
-    computerMove = 's'
-    print('SCISSORS')
+    # Display what the computer chose
+    randomNumber = random.randint(1, 3)
+    if randomNumber == 1:
+        computerMove = 'r'
+        print('ROCK')
+    elif randomNumber == 2:
+        computerMove = 'p'
+        print('PAPER')
+    elif randomNumber == 3:
+        computerMove = 's'
+        print('SCISSORS')
 
-# Display and record the win/loss/tie:
-if playerMove == computerMove:
-    print('It is a tie!')
-    ties = ties + 1
-elif playerMove == 'r' and computerMove == 's':
-    print('You win!')
-    wins = wins + 1
-elif playerMove == 'p' and computerMove == 'r':
-    print('You win!')
-    wins = wins + 1
-elif playerMove == 's' and computerMove == 'p':
-    print('You win!')
-    wins = wins + 1
-elif playerMove == 'r' and computerMove == 'p':
-    print('You lose!')
-    losses = losses + 1
-elif playerMove == 'p' and computerMove == 's':
-    print('You lose!')
-    losses = losses + 1
-elif playerMove == 's' and computerMove == 'r':
-    print('You lose!')
-    losses = losses + 1
+    # Display and record the win/loss/tie:
+    if playerMove == computerMove:
+        print('It is a tie!')
+        ties = ties + 1
+    elif playerMove == 'r' and computerMove == 's':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'p' and computerMove == 'r':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 's' and computerMove == 'p':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'r' and computerMove == 'p':
+        print('You lose!')
+        losses = losses + 1
+    elif playerMove == 'p' and computerMove == 's':
+        print('You lose!')
+        losses = losses + 1
+    elif playerMove == 's' and computerMove == 'r':
+        print('You lose!')
+        losses = losses + 1
--- rpsGameMyAttempt.py
+++ rpsGameMyAttempt.py
@@ -46,6 +46,5 @@ while True:
     else:
         print('You lose!')
         losses = losses + 1
-    
 
 
